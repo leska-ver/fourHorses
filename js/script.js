ead19519d34dad143ba77d021303e10acf8ad01a
leska-ver <lyaisan.333@yandex.ru>
2024-04-26 20:47:13 +0500

    Шахматный клуб - 4 коня

--- js/script.js
+++ js/script.js
@@ -43,4 +43,18 @@ document.addEventListener('DOMContentLoaded', function() {
   
 
 
+  // Плавный скролл по якорям. В любое место можно кинуть.
+  const smoothLinks = document.querySelectorAll('a[href^="#"]');
+  for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+      e.preventDefault();
+      const id = smoothLink.getAttribute('href');
+
+      document.querySelector(id).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+      });
+    });
+  };
+
 });  
